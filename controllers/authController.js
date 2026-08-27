e84f32760e7225a59298f85feb7c1ce1262df8cd
abdulmoizsoomrocs <abdulmoizsoomrocs@gmail.com>
2026-08-27 19:17:34 +0500

    added loginUser

--- controllers/authController.js
+++ controllers/authController.js
@@ -23,9 +23,39 @@ const getUsers = async(req , res) =>{
     res.send(user)
 }
 
+const loginUser = async(req , res) => {
 
+    const email   =  req.body.email
+    const password = req.body.password 
+
+    const user = await User.findOne({email: email})
+
+    if(!(user)){
+       return res.status(401).json({
+        message: "Invalid email or password"
+        })
+    }
+
+    const found = await bcrypt.compare(
+        password , 
+        user.password
+    )
+
+    if(!found){
+        return res.status(401).json({
+        message: "Invalid email or password"
+        })
+    }
+    return res.status(200).json({
+    message: "Login successful",
+    email : email
+    });
+
+
+}
 
 module.exports = {
   createUser,
-  getUsers
+  getUsers,
+  loginUser
 }
--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -1,8 +1,8 @@
-const {createUser, getUsers} = require("../controllers/authController")
+const {createUser, getUsers , loginUser} = require("../controllers/authController")
 const express = require("express")
 const router = express.Router()
 
 router.post("/register" , createUser),
 router.get("/getAllUsers" , getUsers)
-
+router.post("/login", loginUser)
 module.exports = router;
